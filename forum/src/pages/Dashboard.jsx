df134f5b71cd7e9bed17de031e99c61cc85d58f5
AMAN KUMAR CHHARI <140314215+chhari07@users.noreply.github.com>
2025-03-09 20:33:05 +0530

    Update Dashboard.jsx

--- forum/src/pages/Dashboard.jsx
+++ forum/src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
 
   useEffect(() => {
     setUser(auth.currentUser);
-    fetch("http://localhost:5000/api/questions")
+    fetch("https://varta-laab.onrender.com/api/questions")
       .then((res) => res.json())
       .then((data) => setQuestions(data))
       .catch((error) => console.error("Error fetching questions:", error));
